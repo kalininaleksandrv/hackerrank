a14294bf1c6af63b979e25fc8d95a4222e9ebbf7
kalininaleksandrv <kalinin.aleksandr.v@gmail.com>
2020-10-04 19:37:16 +0300

    init commit

---
+++ src/main/java/H001_v01_Solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class H001_v01_Solution {
+
+    public void getSolution() {
+        System.out.println("enter two digits");
+        Scanner in = new Scanner(System.in);
+        int a, b;
+        a = in.nextInt();
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+    }
+
+    private int solveMeFirst(int a, int b) {
+        System.out.println("result is ");
+        return a+b;
+    }
+}
